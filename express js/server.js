ebc6ba9be3f79f5f676cbd4dd3e97b581be6c4ba
imchirag22 <im.chirag.20@gmail.com>
2025-08-29 14:24:02 +0530

    db setup with schema and folder good practice

--- express js/server.js
+++ express js/server.js
@@ -1,15 +1,28 @@
-const express = require('express') // importing a express server and saving in a variable
+import { connectDB } from './src/config/db.js'
+import express from 'express' // importing a express server and saving in a variable
+
 const app = express() // calling a new instance of an express server and assigned the instance to a const 'app' object which we will use to configure server, assign routes and more. This the central point of the backend
 
 // to tanslate the user raw data into json readable format
 app.use(express.json()) // type of middleware
 
+try {
+    connectDB();
+    console.log("database connected")
+} catch (error) {
+    console.log(error)
+}
+const myLogger = (req,res,next) => {
+    console.log(`Recieved : ${req.method} for ${req.url}`)
+    next()
+}
+// app.use(myLogger)
 //app.get(routes,(res/req){} a callback function which runs whenever get request is made. res=msg from client & res=msg from server)
 app.get('/',(req,res) => {
   res.send('Hello World!')
 })
 // app.post()
-app.post('/api/users',(req,res) =>{
+app.post('/api/users',myLogger,(req,res) =>{
     console.log('body',req.body) 
     res.json(req.body)
 })
---
+++ express js/src/users/model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        passsword: {
+            type: String,
+            required: true,
+        },
+        avatar: {
+            type: String,
+            required: false,
+        }
+    },
+    {timestamps: true}
+)
+export const User = mongoose.model("User",userSchema)
